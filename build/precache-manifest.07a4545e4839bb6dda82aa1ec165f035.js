62139f440920aeec57e87dbf4787797b3fdece45
Neşat Talha Kurtuluş <nesattalhakurtulus@Nesat-MacBook-Air.local>
2022-06-28 16:25:21 +0300

    last first try hopefully

---
+++ build/precache-manifest.07a4545e4839bb6dda82aa1ec165f035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8688b5a36f34f0f02227",
+    "url": "/react-app-trial/static/css/main.5ea6d1d9.chunk.css"
+  },
+  {
+    "revision": "8688b5a36f34f0f02227",
+    "url": "/react-app-trial/static/js/main.8688b5a3.chunk.js"
+  },
+  {
+    "revision": "72dc6a3fa1eb8345d146",
+    "url": "/react-app-trial/static/js/1.72dc6a3f.chunk.js"
+  },
+  {
+    "revision": "dcf073acbb0618b97aee",
+    "url": "/react-app-trial/static/js/runtime~main.dcf073ac.js"
+  },
+  {
+    "revision": "73bce11605b271be64c0dfd34b594dd2",
+    "url": "/react-app-trial/index.html"
+  }
+];
